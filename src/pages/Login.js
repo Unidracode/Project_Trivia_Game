e26e4290d1fa72eaaf1db73492643d6b2c706d6b
Gustavo <unidragorn@gmail.com>
2022-11-09 18:22:58 -0300

    R01 feat: FINALIZADO

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,12 +2,33 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Login extends React.Component {
+  state={
+    name:'',
+    email:'',
+    btnDisabled:true,
+  }
+
+  handleChange = (event) => {
+    const { name, value }=event.target;
+    this.setState({ [name]: value }, () => this.validateBtn());
+  }
+
+  validateBtn = () => {
+    const { name, email, btnDisabled }=this.state;
+    const validateName = name.length > 0;
+    const validateEmail = email.length > 0;
+    if(validateName && validateEmail) {
+      this.setState({ btnDisabled : false });
+    }
+  }
+
   render() {
+    const { name, email, btnDisabled }=this.state;
     return(
       <div>
-        <input type="text" name="" value="" placeholder='Nome' data-testid="input-player-name" />
-        <input type="email" name="" value="" placeholder='Email' data-testid="input-gravatar-email" />
-        <button type="button" >
+        <input type="text" name="name" value={ name } placeholder='Nome' onChange={ this.handleChange } data-testid="input-player-name" />
+        <input type="email" name="email" value={ email } placeholder='Email' onChange={ this.handleChange } data-testid="input-gravatar-email" />
+        <button type="button" disabled={ btnDisabled } data-testid="btn-play" >
           Play
         </button>
       </div>
