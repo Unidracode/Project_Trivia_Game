9c92c4235fa6eb7ede452071ba88a696744a7e09
Gustavo <unidragorn@gmail.com>
2022-11-10 17:52:21 -0300

    R06 feat: faz requisição

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import triviaAPI from '../helpers/triviaAPI';
 
 class Game extends Component {
+  state = {
+    response:{}
+  }
+  async componentDidMount () {
+    const token = localStorage.getItem('token');
+    const getAPI = await triviaAPI(token);
+    this.setState({ response:getAPI });
+    console.log(getAPI);
+  }
+
   render() {
     return (
       <Header />
